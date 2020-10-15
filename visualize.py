fbca5731507263fb58c0800a829aaad7a089f6cd
Hans Kirchner <hans-kirchner@gmx.de>
2020-10-16 00:01:48 +0200

    Update visualize script to work with multiple csv files

--- visualize.py
+++ visualize.py
@@ -2,11 +2,19 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import sys
+import os
 
 def main():
-    df = pd.read_csv(sys.argv[1], header=None, names=["ts", "cnt", "max"], index_col="ts", parse_dates=["ts"])
-    df.plot(ylim=[0, df["max"].max()*1.1], grid=True)
+    for file in sys.argv[1:]:
+        df = pd.read_csv(file, header=None, names=["ts", "cnt", "max"], index_col="ts", parse_dates=["ts"])
+        df.index = df.index.tz_convert('Europe/Berlin')
+        df["cnt"].plot(grid=True, label=os.path.splitext(os.path.basename(file))[0])
+    plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1))
+    plt.ylabel("Visitors")
+    plt.xlabel("Date/Time")
+    plt.tight_layout()
     plt.show()
+    #plt.savefig("baeder.svg")
 
 if __name__ == "__main__":
     main()
